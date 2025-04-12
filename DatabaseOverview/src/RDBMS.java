476131c315803c581915349768ad5c5b00c3f14d
gdghanu-mahn105 <gdschanu.manhdd1005@gmail.com>
2025-04-12 21:30:31 +0700

    [core] Relational Database Management System- RDBMS

---
+++ DatabaseOverview/src/RDBMS.java
@@ -0,0 +1,40 @@
+public class RDBMS {
+    /*
+Hệ quản trị cơ sở dữ liệu (RDBMS)
+Relational Database Management System
+
+Cấu trúc: tạo nên từ nhiều bảng có liên kết với nhau.
+- Sử dụng cho mô hình quan hệ
+- Yêu cầu cơ sở hạ tầng: cần nhiều phần mềm và hệ thống phần cứng cao
+- Cung cấp cơ sở dữ liệu phân tán
+- Phù hợp với tất cr dữ liệu từ nhỏ tới lớn\
+- Dựa trên ACID để đảm bảo tính nhất quán
+- Chuẩn hoá dữ liệu
+- Quản lí mối quan hệ giữa các bảng dữ liệu
+
+Hoạt động:
+Bảng dữ liệu có mối quan hệ với nhau, mỗi bảng có từng thuộc tính, kiểu đữ liệu, khoá chính duy nhất để xác định
+Ràng buộc:
+- Primary keys: mỗi bảng sẽ chỉ có 1 khoá chính và giá trị của trường này không được null.
+    Bộ giá trị của khoá chính luôn là duy nhất, không được trùng lặp
+- Foreign keys: khoá phụ có thể dùng để liên kết hai bảng với nhau
+- Not null: dữ liệu không thể để trống
+- Điều kiện check: dữ liệu khi nhập vào phải thoả mãn điều kiện chính xác và duy nhất (do người dùng thiết lập các kiểu phạm vị….)
+
+Ứng dụng:
+- Dùng trên hầu hết các tổ chức toàn thế giới
+- Hệ thống bán hàng, làm việc với khách hàng
+- Ngân hàng, nhân sự, kế toán
+- Công cụ hỗ trợ tạo lập RDBMS: Oracle, MySQL, Microsoft SQL server, IBM…
+
+Hệ quản trị cơ sở dữ liệu phổ biến:
+- MySQL: một hệ quản trị cơ sở dữ liệu mã nguồn mở, phổ biến cho các ứng dụng web
+- PostgreSQL: mã nguồn mở, hỗ trợ nhiều tinhs năng nâng cao
+- Oracle Database: hệ quản trị cơ sở dữ liệu thương mại, khả năng xử lý khối lượng lớn dữ liệu
+- Microsoft SQL Server: thuộc microsoft, thường dùng trong các ứng dụng doanh nghiệp
+
+Tối ưu hóa truy vấn: Sử dụng các chỉ mục, phân vùng dữ liệu, và tối ưu hóa cấu trúc bảng để cải thiện hiệu suất truy vấn.
+Sao lưu và phục hồi: RDBMS thường cung cấp các công cụ để sao lưu dữ liệu và phục hồi trong trường hợp mất mát dữ liệu.
+
+     */
+}
